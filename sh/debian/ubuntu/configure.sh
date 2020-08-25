cf2c048e81445bf16336f0c8666eec0cba5b8b6a
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-08-25 15:44:54 +0600

    fix: update vscode

--- sh/debian/ubuntu/configure.sh
+++ sh/debian/ubuntu/configure.sh
@@ -180,8 +180,8 @@ apt-get install -y sublime-text sublime-merge libgtk2.0-0
 
 # Install VSCode
 
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | apt-key add -
-echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' >> /etc/apt/sources.list.d/vscodium.list
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | tee /etc/apt/sources.list.d/vscodium.list
 
 apt-get update
 apt-get install -y codium
